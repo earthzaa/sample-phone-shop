2d6bf2c8dc5ce29124ef9fcd00739d5492f6840c
Thirasan Minthakhat <earthzaa00@gmail.com>
2021-10-08 15:25:23 +0700

    update v1.1 add features

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ let carouselRight = document.getElementById('carousel-right');
 let productList = document.getElementById('product-list');
 let cartBtn = document.getElementById('cart');
 let cartList = document.getElementById('cart-list');
+let notiContainer = document.getElementById('noti-container');
+let notiInfo = document.getElementById('noti-info');
+let totalInCart = document.getElementById('total-items');
 
 let banners = [];
 let carouselIndex = 0;
@@ -18,8 +21,24 @@ let isOpenCart = false;
 
 const formatPrice = (price = '') => new Intl.NumberFormat('en-En').format(price);
 
+const updateTotalInCart = () => {
+    const length = cart.length;
+
+    totalInCart.innerHTML = length > 9 ? `${9}+` : length;
+};
+
 const addProductToCart = (index = 0) => {
-    cart.push(products[index]);
+    const product = products[index];
+    cart.push(product);
+
+    updateTotalInCart();
+    notiContainer.innerHTML = '';
+    notiContainer.setAttribute('style', 'display: none');
+    notiContainer.setAttribute('style', 'display: block')
+    notiInfo.innerHTML = `add ${product?.name || ''} - $${(product?.price - product?.discount) || 0} to Cart !`
+
+    notiContainer.appendChild(notiInfo);
+    setTimeout(() => notiContainer.setAttribute('style', 'display: none'), 1500);
 };
 
 const setProducts = () => {
@@ -52,6 +71,7 @@ const setProducts = () => {
 
         let btn = document.createElement('button');
         btn.setAttribute('class', 'card-btn');
+        btn.setAttribute('name', 'btn');
         btn.innerHTML = `Order`;
         btn.addEventListener('click', () => addProductToCart(index));
 
@@ -96,8 +116,25 @@ const clickArrowCarousel = (event, direction = CAROUSEL_DIRECTION.LEFT) => {
     carouselImg.src = banners[carouselIndex]?.img;
 };
 
-const handleClickCart = () => {
-    toggleCartList();
+const removeProductFromCart = (index = 0) => {
+    cart.splice(index, 1);
+    updateTotalInCart();
+    updateCartList();
+}
+
+const updateCartList = () => {
+    cartList.innerHTML = '';
+
+    if (cart.length === 0) {
+
+        let noData = document.createElement('h4');
+        noData.setAttribute('class', 'no-data-center');
+        noData.innerHTML = 'No products in cart...'
+
+        cartList.appendChild(noData);
+
+        return null;
+    }
 
     cart.forEach((product, index) => {
         let div = document.createElement('div');
@@ -111,18 +148,29 @@ const handleClickCart = () => {
         detail.setAttribute('class', 'cart-detail');
 
         let name = document.createElement('span');
-        name.innerHTML = `${product?.name || ''}`;
+        name.innerHTML = `${index + 1}). ${product?.name || ''}`;
 
         let price = document.createElement('span');
-        price.innerHTML = `$${product?.price || 0}`;
+        price.innerHTML = `$${(product?.price - product?.discount) || 0}`;
+
+        let iconDel = document.createElement('i');
+        iconDel.setAttribute('class', 'fa fa-times fa-1x icon-delete');
+        iconDel.setAttribute('name', 'icon-delete');
+        iconDel.addEventListener('click', () => removeProductFromCart(index));
 
         detail.appendChild(name);
         detail.appendChild(document.createElement('br'));
         detail.appendChild(price);
         div.appendChild(img);
         div.appendChild(detail);
+        div.appendChild(iconDel);
         cartList.appendChild(div);
     });
+}
+
+const handleClickCart = () => {
+    toggleCartList();
+    updateCartList();
 };
 
 const toggleCartList = () => {
@@ -133,9 +181,13 @@ const toggleCartList = () => {
 };
 
 const handleClickOutsideCartList = (event) => {
-    const { id } = event.target;
+    const { id, className } = event.target;
 
-    if (id !== 'cart' && isOpenCart) toggleCartList();
+    if (
+        id !== 'cart'
+        && isOpenCart
+        && className !== 'fa fa-times fa-1x icon-delete'
+    ) toggleCartList();
 };
 
 carouselLeft.addEventListener('click', (e) => clickArrowCarousel(e, CAROUSEL_DIRECTION.LEFT));
